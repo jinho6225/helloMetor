192dcf13600a2a527d07186b49ca5bf6f2692002
jinho.myung <jinho6225@hotmail.com>
2022-02-21 15:24:14 -0800

    update port

--- server/index.js
+++ server/index.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3300
+const port = process.env.PORT || 3300
 const path = require('path');
 const cors = require('cors');
 const router = require('./router')
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
 app.use('/', router);
 // app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
